2b32f064c80152cbdebe8f06ae0231525382deda
MouraYuri <mourayuri339@gmail.com>
2020-11-06 23:28:31 -0300

    initial commit

---
+++ iOSChallenge/Views/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  iOSChallenge
+//
+//  Created by Yuri Moura on 06/11/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
